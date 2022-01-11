1d4c27aa4ec344db0275198456a82ec295ff4c36
wikSak <wika.sak@gmail.com>
2022-01-11 19:07:19 +0100

    start of increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends React.Component {
-    render() { 
-        return <h1>Hello World!</h1>;
-    }
+  state = {
+    count: 1,
+    tags: [],
+  };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>Empty array!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+//  constructor() {
+//      super();
+//      this.handleIncrement = this.handleIncrement.bind(this);
+//  }
+  handleIncrement = () => {
+  this.setState({count: this.state.count + 1})
+  }
+
+  render() {
+    return (
+      <div>
+          <span>{this.state.count}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+        <span></span>
+        {/* {this.state.tags.length === 0 && "It is empty. Create!"} */}
+        {this.renderTags()}
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
